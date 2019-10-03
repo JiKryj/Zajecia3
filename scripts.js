3ffcac5940d664682e3ebbdda11f17578f329744
JiKryj <44032845+JiKryj@users.noreply.github.com>
2019-10-03 19:53:02 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,42 @@
-console.log("skrypty załadowane!")
+const owce = [
+{
+    name : "owca2",
+    color : "red" ,
+    koza : 7,
+},
+{
+    name : "owca3",
+    color : "white" ,
+    koza : 6,
+},
+{
+    name : "owca4",
+    color : "black" ,
+    koza : 9,
+},
+{
+    name : "owca5",
+    color : "red" ,
+    koza : 8,
+},
+{
+    name : "owca6",
+    color : "green" ,
+    koza : 3,
+},
+]
+
+
+const reprezentacjaOwiec = owce.map( owca =>`
+<div class="owca">
+<h3>${owca.name}</h3>
+<h3>${owca.color}</h3>
+<h3>${owca.koza}</h3>
+</div>
+`)
+
+const tekstDoWstawienia = reprezentacjaOwiec.join("");
+
+const miejsceNaTekst = document.querySelector("#miejsceNaOwce")
+
+miejsceNaTekst.innerHTML = tekstDoWstawienia;
